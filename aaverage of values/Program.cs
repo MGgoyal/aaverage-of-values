28d631c7376b67caf169e79f51b763b064e6233b
tanvi goyal <tanvi goyal@DESKTOP-Q4I1B7T>
2023-08-28 14:08:13 +0530

    Add project files.

---
+++ aaverage of values/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+
+namespace opdracht2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = new string[10] { "7", "8", "4", "6", "5,5", "7,5", "2", "3,3", "4.9", "8.9" };
+            for (int i = 0, i <= array.Count, i++)
+            {
+                total += array[i]
+            }
+
+            float average = total / array.Count
+        }
+    }
